c4541a3b0130dba9275f15ddbea31b413af40f83
Dzhemilya <Dzhemilya.giz@yandex.ru>
2021-08-25 16:30:39 +0300

    update second task from second week.

--- week02/ex2.c
+++ week02/ex2.c
@@ -4,13 +4,13 @@
 
 int main () {
     char *USER_INPUT;
-    int chr;
-    int SIZE = 32;
+
+    int SIZE = 4096;
 
     USER_INPUT = (char *)malloc(SIZE * sizeof(char));
 
     printf("Enter string to reverse: ");
-    chr = getline(&USER_INPUT,&SIZE,stdin);
+    getline(&USER_INPUT,&SIZE,stdin);
     printf("Reversed string: ");
     for (int i = strlen(USER_INPUT); i>=0; i--) {
 	if (USER_INPUT[i] != '\n') {
